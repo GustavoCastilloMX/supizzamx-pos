56af5837f771a527bd1dcc53591c30830de08563
tavoGeek <gcnctz@gmail.com>
2020-08-07 21:23:06 -0500

    :gear: CONFIGURACION FUENTE Y PESOS DE LA FUENTE

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -1,14 +1,19 @@
 <template>
   <v-container>
     <h1>Inicio</h1>
+    <p class="font-weight-black">Black text.</p>
+    <p class="font-weight-bold">Bold text.</p>
+    <p class="font-weight-medium">Medium weight text.</p>
+    <p class="font-weight-regular">Normal weight text.</p>
+    <p class="font-weight-light">Light weight text.</p>
+    <p class="font-weight-thin">Thin weight text.</p>
+    <p class="font-italic">Italic text.</p>
   </v-container>
 </template>
 
 <script>
-
 export default {
-  name: 'InicioView',
-  components: {
-  }
-}
+  name: "InicioView",
+  components: {},
+};
 </script>
